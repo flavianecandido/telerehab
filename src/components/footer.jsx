48d41c90856afa7685254bde430042bdbcf1ab78
Flaviane Candido <flavianealvescandido@yahoo.com.br>
2023-11-22 23:26:38 -0300

    alinhamento footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ import { ROUTER_CONFIG } from "../config/constants"
 export function Footer() {
     return (
         <footer className="bg-zinc-100 py-16">
-            <div className="flex gap-14 px-20">
+            <div className="flex gap-14 px-96">
                 <div className="">
                     <div className="flex justify-between gap-3 items-center">
                         <Stethoscope size={36} className="text-primary" />
@@ -17,20 +17,22 @@ export function Footer() {
                         <Link><Linkedin size={26} /></Link>
                     </div>
                 </div>
-                <div className="ml-40 mt-12">
-                    <ul>
-                        <li className="font-bold text-neutral-500 text-2xl">SERVIÇOS</li>
-                        <Link to={ROUTER_CONFIG.ACCESS} className="font-medium text-neutral-500 text-lg hover:underline"><li>Acessar Consulta</li></Link>
-                        <Link to={ROUTER_CONFIG.BOOKING} className="font-medium text-neutral-500 text-lg hover:underline"><li>Agendamento</li></Link>
-                    </ul>
-                </div>
-                <div className="mt-12">
-                    <ul>
-                        <li className="font-bold text-neutral-500 text-2xl">ATENDIMENTO</li>
-                        <Link className="font-medium text-neutral-500 text-lg hover:underline"><li>Canais de Saúde</li></Link>
-                        <Link className="font-medium text-neutral-500 text-lg hover:underline"><li>Ouvidoria</li></Link>
-                        <Link className="font-medium text-neutral-500 text-lg hover:underline"><li>Dúvidas Frequentes</li></Link>
-                    </ul>
+                <div className="flex gap-28">
+                    <div className="ml-64 mt-12">
+                        <ul>
+                            <li className="font-bold text-neutral-500 text-2xl">SERVIÇOS</li>
+                            <Link to={ROUTER_CONFIG.ACCESS} className="font-medium text-neutral-500 text-lg hover:underline"><li>Acessar Consulta</li></Link>
+                            <Link to={ROUTER_CONFIG.BOOKING} className="font-medium text-neutral-500 text-lg hover:underline"><li>Agendamento</li></Link>
+                        </ul>
+                    </div>
+                    <div className="mt-12">
+                        <ul>
+                            <li className="font-bold text-neutral-500 text-2xl">ATENDIMENTO</li>
+                            <Link className="font-medium text-neutral-500 text-lg hover:underline"><li>Canais de Saúde</li></Link>
+                            <Link className="font-medium text-neutral-500 text-lg hover:underline"><li>Ouvidoria</li></Link>
+                            <Link className="font-medium text-neutral-500 text-lg hover:underline"><li>Dúvidas Frequentes</li></Link>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div className=" w-[100%] absolute bg-primary mt-14">
